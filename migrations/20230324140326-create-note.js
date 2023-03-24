91973d4236966e5ad45e06260ebf33ed17bd49e1
Agung Tri Utomo <agungtriu@gmail.com>
2023-03-24 21:23:49 +0700

    init app

---
+++ migrations/20230324140326-create-note.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('notes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      imageType: {
+        type: Sequelize.STRING
+      },
+      imageName: {
+        type: Sequelize.STRING
+      },
+      imageData: {
+        type: Sequelize.BLOB
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      projectId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('notes');
+  }
+};
