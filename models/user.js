eb7be51f8c6ce3b3f9ff0bde6f87e1c9fb1dcc22
Lingga <linggaadiatmdj@gmail.com>
2023-03-25 11:06:30 +0700

    add associate user to profile

--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
     /**
@@ -11,17 +9,21 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      user.belongsToMany(models.product, {through: models.projectUser})
+      user.hasOne(models.profile);
+      user.belongsToMany(models.product, { through: models.projectUser });
     }
   }
-  user.init({
-    username: DataTypes.STRING,
-    name: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
+  user.init(
+    {
+      username: DataTypes.STRING,
+      name: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "user",
+    }
+  );
   return user;
 };
